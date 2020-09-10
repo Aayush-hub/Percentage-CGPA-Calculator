5f756b97fd71292e8dc1e46a86d3ce9486375e30
AbhijayJain <64387054+Abhijay007@users.noreply.github.com>
2020-09-10 13:22:59 +0530

    Update CGPA.js
    Additional changes

--- CGPA-calc/CGPA.js
+++ CGPA-calc/CGPA.js
@@ -6,42 +6,42 @@ function fn1() {
     var percentage = (num1 / num2) * 100
     percentage = percentage.toFixed(2);
     //For Percentage field
-    var per = document.getElementById("percentage-got").placeholder = "Percentage : " + percentage + "%";
+    var per = document.getElementById("percentage-got").placeholder = percentage + "%";
     var Cgpa = percentage / 9.5;
     Cgpa = Cgpa.toFixed(1);
     //For CGPA field
-    var GPA = document.getElementById("Cgpa-got").placeholder = "CGPA : " + Cgpa;
+    var GPA = document.getElementById("Cgpa-got").placeholder = Cgpa;
     //For GRADE field
     var grade;
 
     if (percentage >= 95) {
-        GPA = document.getElementById("Cgpa-got").placeholder = "CGPA : 10";;
-        grade = document.getElementById("grade").placeholder = "GRADE : 'A+'";
+        GPA = document.getElementById("Cgpa-got").placeholder = "10";;
+        grade = document.getElementById("grade").placeholder = " 'A+'";
 
     } else if (percentage >= 90) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'A+'";
+        grade = document.getElementById("grade").placeholder = " 'A+'";
     } else if (percentage > 80) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'A'";
+        grade = document.getElementById("grade").placeholder = " 'A'";
     } else if (percentage > 75) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'B+'";
+        grade = document.getElementById("grade").placeholder = " 'B+'";
     } else if (percentage > 70) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'B'";
+        grade = document.getElementById("grade").placeholder = " 'B'";
     } else if (percentage > 60) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'C'";
+        grade = document.getElementById("grade").placeholder = " 'C'";
     } else if (percentage > 50) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'D'";
+        grade = document.getElementById("grade").placeholder = " 'D'";
     } else if (percentage > 40) {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'E'";
+        grade = document.getElementById("grade").placeholder = " 'E'";
     } else {
         Cgpa = GPA
-        grade = document.getElementById("grade").placeholder = "GRADE : 'F'";
+        grade = document.getElementById("grade").placeholder = " 'F'";
     }
 
 }
